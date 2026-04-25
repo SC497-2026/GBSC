55195db4ca4d9a5b71eab7af40c427c539473cc8
Kyungkuk-Min <kfm5604@psu.edu>
2026-04-25 16:57:36 -0400

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,7 +4,4 @@ def test_estimate():
     ladder_sizes = [10000, 8000, 6000, 4000]
     ladder_distances = [10, 15, 20, 25]
     sample_distance = 18
-
-    result = estimate_band_size(ladder_sizes, ladder_distances, sample_distance)
-
-    assert result > 0
+    Estimated sample band size: 6421.52 bp
